71cd250fed4962ee90a8a260d7b8290d2a58a68b
christophe-g <christophe.geiser@gmail.com>
2020-10-26 14:47:12 +0100

    Improved accessibility for invalid fields

--- src/extension/pwi-textarea.js
+++ src/extension/pwi-textarea.js
@@ -1,23 +1,26 @@
+import {html} from 'lit-element';
+import { ifDefined } from 'lit-html/directives/if-defined.js';
+import { live } from 'lit-html/directives/live.js';
 import { TextArea } from '@material/mwc-textarea';
 import TwoWayBinding from './two-way-binding.js';
 import OverrideTextfield from './override-textfield.js';
 import OverrideTextfieldReadonly from './override-textfield-readonly.js';
-import {css} from 'lit-element';
+import { css } from 'lit-element';
 
 /**
  * extenstion of mwc-textfield emiting a value-changed event when
  * value changes
  */
-class PwiTextArea extends 
-  TwoWayBinding(
-    OverrideTextfield(
-      OverrideTextfieldReadonly(
-        TextArea))) {
+class PwiTextArea extends
+TwoWayBinding(
+  OverrideTextfield(
+    OverrideTextfieldReadonly(
+      TextArea))) {
 
   static get styles() {
     return [
       super.styles,
-      
+
       css `
         :host([ripple]) .mdc-text-field {
           background: transparent !important;
@@ -60,6 +63,42 @@ class PwiTextArea extends
     } else {
       style.resize = null;
     }
+  }
+
+  // Note(cg): override to improve aria support.
+  renderInput() {
+    const minOrUndef = this.minLength === -1 ? undefined : this.minLength;
+    const maxOrUndef = this.maxLength === -1 ? undefined : this.maxLength;
+    const autocapitalizeOrUndef = this.autocapitalize ?
+      this.autocapitalize :
+      undefined;
+    const showValidationMessage = this.validationMessage && !this.isUiValid;
+    
+    return html `
+      <textarea
+          aria-labelledby="label"
+          aria-controls="${ifDefined(this.shouldRenderHelperText ? 'helper-text' : undefined)}"
+          aria-describedby="${ifDefined(this.focused || this.helperPersistent || showValidationMessage ?
+            'helper-text' :
+            undefined)}"
+         aria-errormessage="${ifDefined(showValidationMessage ? 'helper-text' : undefined)}"
+         aria-invalid="${ifDefined(this.isUiValid ? undefined : 'true')}"
+          class="mdc-text-field__input"
+          .value="${live(this.value)}"
+          rows="${this.rows}"
+          cols="${this.cols}"
+          ?disabled="${this.disabled}"
+          placeholder="${this.placeholder}"
+          ?required="${this.required}"
+          ?readonly="${this.readOnly}"
+          minlength="${ifDefined(minOrUndef)}"
+          maxlength="${ifDefined(maxOrUndef)}"
+          name="${ifDefined(this.name === '' ? undefined : this.name)}"
+          inputmode="${ifDefined(this.inputMode)}"
+          autocapitalize="${ifDefined(autocapitalizeOrUndef)}"
+          @input="${this.handleInputChange}"
+          @blur="${this.onInputBlur}">
+      </textarea>`;
   }
 }
 
--- src/extension/pwi-textfield.js
+++ src/extension/pwi-textfield.js
@@ -1,3 +1,8 @@
+import { html } from 'lit-element';
+import { nothing } from 'lit-html';
+import { ifDefined } from 'lit-html/directives/if-defined.js';
+import { live } from 'lit-html/directives/live.js';
+import { classMap } from 'lit-html/directives/class-map.js';
 import { TextField } from '@material/mwc-textfield';
 import TwoWayBinding from './two-way-binding.js';
 import OverrideTextfield from './override-textfield.js';
@@ -9,11 +14,85 @@ import OverrideTextfieldReadonly from './override-textfield-readonly.js';
  * value changes
  */
 class PwiTextField extends
-  TwoWayBinding(
-    OverrideTextfield(
-        OverrideTextfieldValidityMessage(
-          OverrideTextfieldReadonly(
-            TextField)))) {
+TwoWayBinding(
+  OverrideTextfield(
+    OverrideTextfieldValidityMessage(
+      OverrideTextfieldReadonly(
+        TextField)))) {
+
+
+  // Note(cg): improved aria support on text field: 
+  // aria-invalie is set to invalid;
+  // aria-errormessage replaces aria-errortext
+  // .
+  renderInput() {
+    const minOrUndef = this.minLength === -1 ? undefined : this.minLength;
+    const maxOrUndef = this.maxLength === -1 ? undefined : this.maxLength;
+    const autocapitalizeOrUndef = this.autocapitalize ?
+      this.autocapitalize :
+      undefined;
+    const showValidationMessage = this.validationMessage && !this.isUiValid;
+    // TODO: live() directive needs casting for lit-analyzer
+    // https://github.com/runem/lit-analyzer/pull/91/files
+    // TODO: lit-analyzer labels min/max as (number|string) instead of string
+    return html `
+      <input
+          aria-labelledby="label"
+          aria-controls="${ifDefined(this.shouldRenderHelperText ? 'helper-text' : undefined)}"
+          aria-describedby="${ifDefined(this.focused || this.helperPersistent || showValidationMessage ?
+            'helper-text' :
+            undefined)}"
+         aria-errormessage="${ifDefined(showValidationMessage ? 'helper-text' : undefined)}"
+         aria-invalid="${ifDefined(this.isUiValid ? undefined : 'true')}"
+          class="mdc-text-field__input"
+          type="${this.type}"
+          .value="${live(this.value)}"
+          ?disabled="${this.disabled}"
+          placeholder="${this.placeholder}"
+          ?required="${this.required}"
+          ?readonly="${this.readOnly}"
+          minlength="${ifDefined(minOrUndef)}"
+          maxlength="${ifDefined(maxOrUndef)}"
+          pattern="${ifDefined(this.pattern ? this.pattern : undefined)}"
+          min="${ifDefined(this.min === '' ? undefined : this.min)}"
+          max="${ifDefined(this.max === '' ? undefined : this.max)}"
+          step="${ifDefined(this.step === null ? undefined : this.step)}"
+          size="${ifDefined(this.size === null ? undefined : this.size)}"
+          name="${ifDefined(this.name === '' ? undefined : this.name)}"
+          inputmode="${ifDefined(this.inputMode)}"
+          autocapitalize="${ifDefined(autocapitalizeOrUndef)}"
+          @input="${this.handleInputChange}"
+          @focus="${this.onInputFocus}"
+          @blur="${this.onInputBlur}">`;
+  }
+
+
+  // Note(cg): added aria-live. and role
+  renderHelperText(charCounterTemplate = nothing) {
+    if (!this.shouldRenderHelperText) {
+      return nothing;
+    }
+    const showValidationMessage = this.validationMessage && !this.isUiValid;
+    const classes = {
+      'mdc-text-field-helper-text--persistent': this.helperPersistent,
+      'mdc-text-field-helper-text--validation-msg': showValidationMessage,
+    };
+    const role = showValidationMessage ? 'alert' : undefined;
+
+    return html `
+      <div class="mdc-text-field-helper-line">
+        <div
+        id="helper-text"
+        role="${ifDefined(role)}"
+        aria-live="${ifDefined(showValidationMessage ? 'assertive' : undefined)}"
+        aria-hidden="${ifDefined(this.focused || this.helperPersistent || showValidationMessage ?
+            undefined :
+            'true')}"
+        class="mdc-text-field-helper-text ${classMap(classes)}">${showValidationMessage ? this.validationMessage : this.helper}</div>
+        ${charCounterTemplate}
+      </div>
+    `;
+  }
 }
 
 customElements.define('pwi-textfield', PwiTextField);
