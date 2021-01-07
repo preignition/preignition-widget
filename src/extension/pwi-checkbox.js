57f19ead17bd5a882e531f185c4abd817f37afd6
christophe-g <christophe.geiser@gmail.com>
2021-01-07 12:26:44 +0100

    remove aria-checked override

--- src/extension/pwi-checkbox.js
+++ src/extension/pwi-checkbox.js
@@ -1,12 +1,10 @@
 import { Checkbox } from '@material/mwc-checkbox';
-import OverrideCheckboxAriaChecked from './override-checkbox-aria-checked.js' 
-// import TwoWayCheckedBinding from './`t`wo-way-checked-binding.js';
 
 /**
  * extention of mwc-switch emiting a value-changed event when
  * value changes
  */
-class PwiCheckbox extends OverrideCheckboxAriaChecked(Checkbox) {
+class PwiCheckbox extends Checkbox {
   
   // Note(cg): checkbox has _changehandler (unlike switch and radio).
   _changeHandler(e) {
