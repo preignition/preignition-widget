0b93f76fbc884cb13693f0cefd13b961d6be9aa6
christophe-g <christophe.geiser@gmail.com>
2021-01-08 19:29:42 +0100

    Prevent select to fire change when disconnected.

--- src/extension/two-way-select-binding.js
+++ src/extension/two-way-select-binding.js
@@ -12,7 +12,12 @@ export const TwoSelectBinding = (baseElement) => class extends baseElement {
   }
 
   dispatchValueChanged() {
-    this.dispatchEvent(new CustomEvent('value-changed', { detail: { value: this.value }, bubbles: true, composed: true }));
+    // Note(cg): only notify change if the field is still connected. 
+    // Othewise, this can lead to values being changed when the field if removed 
+    // For instance: https://gitlab.com/christophe-g/ida-ta/-/issues/383
+    if (this.isConnected) {
+      this.dispatchEvent(new CustomEvent('value-changed', { detail: { value: this.value }, bubbles: true, composed: true }));
+    }
   }
 };
 
