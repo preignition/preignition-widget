899a75767b36eeb4361054dcdeda0017952ee856
christophe-g <christophe.geiser@gmail.com>
2021-04-14 15:48:58 +0200

    radio and checbox group translation are now under readaloud locale

--- src/form/readaloud-locale.js
+++ src/form/readaloud-locale.js
@@ -8,5 +8,6 @@ export default {
   isTheAnswerTo: 'is the answer to the question',
   countOptions: 'There are {count} options to read. Click "read aloud" again to read them all.',
   chooseOption: 'Choose your answers from the following options',
-  chooseOptions: 'Choose one answer from the following options'
+  chooseOptions: 'Choose one answer from the following options',
+  pleaseSpecify: 'please specify'
  }
--- src/locale.js
+++ src/locale.js
@@ -1,11 +1,10 @@
 import PwiLogIn from './widget/pwi-log-in-locale.js';
-import PwiCheckboxGroup from './form/group/pwi-checkbox-group-locale.js';
-import PwiRadioGroup from './form/group/pwi-radio-group-locale.js';
+// import PwiCheckboxGroup from './form/group/pwi-checkbox-group-locale.js';
+// import PwiRadioGroup from './form/group/pwi-radio-group-locale.js';
 import Readaloud from './form/readaloud-locale.js';
 
  export {
   PwiLogIn,
-  PwiCheckboxGroup,
-  PwiRadioGroup, 
+
   Readaloud
  };
