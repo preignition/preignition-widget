a3b808c58601cbde4cfa0f8aeb1e539d26e969ab
christophe-g <christophe.geiser@gmail.com>
2021-05-05 17:01:20 +0200

    bump dep

--- src/extension/pwi-checkbox.js
+++ src/extension/pwi-checkbox.js
@@ -5,9 +5,9 @@ import { Checkbox } from '@material/mwc-checkbox';
  * value changes
  */
 class PwiCheckbox extends Checkbox {
-  // Note(cg): checkbox has _changehandler (unlike switch and radio).
-  _changeHandler(e) {
-    super._changeHandler(...arguments);
+  // Note(cg): checkbox has handleChange (unlike switch and radio).
+  handleChange(e) {
+    super.handleChange(...arguments);
     this.dispatchEvent(new CustomEvent('checked-changed', { detail: { value: this.checked }, bubbles: true, composed: true }));
   }
 }
